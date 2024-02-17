515c10113a1c7cfb5c8628fa9f9bcf961bd4ad35
guyjbrown <g.j.brown@sheffield.ac.uk>
2024-02-17 18:24:01 +0000

    cycles complete

--- cycles/cycles.js
+++ cycles/cycles.js
@@ -1,36 +1,12 @@
-const CYCLE_NAMES = [
-    "bansuri",
-    "fairvoice",
-    "moogsaw",
-    "nylon",
-    "random",
-];
-
 export async function loadCycles() {
     try {
-        // wait for all promises
-        let promises = CYCLE_NAMES.map(async (name) => {
-            let json = await fetchJSONCycle(name);
-            return json;
-        });
-        let cycles = await Promise.all(promises);
-        // combine all wave tables into single map
-        let combinedCycles = cycles.reduce((acc, cycle) => ({ ...acc, ...cycle }), {});
-        return combinedCycles;
-    } catch (error) {
-        console.error("Error loading cycle: ", error);
-    }
-}
-
-async function fetchJSONCycle(name) {
-    try {
-        const response = await fetch(`./cycles/${name}.json`);
+        const response = await fetch(`./cycles/cycle_defs.json`);
         if (!response.ok) {
-            throw new Error(`HTTP error when fetching cycle: ${response.status}`);
+            throw new Error(`HTTP error when fetching wave cycles: ${response.status}`);
         }
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error("Unable to fetch cycle:", error);
+        console.error("Unable to fetch wave cycles:", error);
     }
 }
